82b9a7d421ffaa98089863ccbc1db14662dc7c04
Sergei Tarasov <www.neon@mail.ru>
2021-11-26 18:37:54 +0500

    add homework08 from Intellij IDEA

---
+++ Homework08/src/com/learning/Main.java
@@ -0,0 +1,88 @@
+package com.learning;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+
+    /**
+     * Процедура выводит на экран массив объектов
+     * @param persons Содержит массив с информацией о людях, который необходимо вывести на экран
+     */
+    public static void readPersonArray(ArrayList<Person> persons) {
+        for (Person person:
+             persons) {
+            System.out.println(person.getName() + ": " + person.getWeight());
+        }
+        System.out.println();
+    }
+
+    /**
+     * Процедура сортирует массив объектов Person по возростанию веса людей, методом сортировки выбором.
+     * @param persons Содержит массив, который необходимо отсортировать
+     * @return Возвращает отсортированный массив
+     */
+    public static ArrayList<Person> sortPersonArray(ArrayList<Person> persons) {
+        Person temp = new Person("temp", 0);
+        for (int i = 0; i < persons.size(); i++) {
+            int indexMinWeight = i;
+            double minWeight = persons.get(i).getWeight();
+            for (int j = i + 1; j < persons.size(); j++) {
+                if (persons.get(j).getWeight() < minWeight) {
+                    indexMinWeight = j;
+                    minWeight = persons.get(j).getWeight();
+                }
+            }
+            temp = persons.get(i);
+            persons.set(i, persons.get(indexMinWeight));
+            persons.set(indexMinWeight, temp);
+        }
+
+        return persons;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Person> persons = new ArrayList<>();
+        persons.add(new Person("Ирина", 48.3));
+        persons.add(new Person("Антон", 71.5));
+        persons.add(new Person("Сергей", 75.1));
+        persons.add(new Person("Елена", 51.5));
+        persons.add(new Person("Илья", 68));
+        persons.add(new Person("Александр", 65.8));
+        persons.add(new Person("Роман", 84.7));
+        persons.add(new Person("Екатерина", 46.3));
+        persons.add(new Person("Анна", 47.4));
+        persons.add(new Person("Владимир", 92));
+
+        readPersonArray(persons);
+        readPersonArray(sortPersonArray(persons));
+    }
+
+    static class Person {
+        private String name;
+        private double weight = 0;
+
+        public Person(String name, double weight) {
+            this.name = name;
+            if (weight > 0 && weight < 500) {
+                this.weight = weight;
+            }
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public double getWeight() {
+            return weight;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setWeight(double weight) {
+            this.weight = weight;
+        }
+    }
+}
