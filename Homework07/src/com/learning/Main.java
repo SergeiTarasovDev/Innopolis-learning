c4bd9cc103af32b172894625677f72d242a51f80
Telion <www.neon@mail.ru>
2021-12-05 09:41:18 +0500

    work on bugs

--- Homework07/src/com/learning/Main.java
+++ Homework07/src/com/learning/Main.java
@@ -19,7 +19,7 @@ public class Main {
         int a = scanner.nextInt();
 
         while (a != -1) {
-            if (a >= -100 && a <=100) {
+            if (a >= -100 && a <= 100) {
                 arrayNumbers.add(a);
             } else {
                 System.out.println("Введенное число не соответствует условию задачи");
@@ -42,8 +42,7 @@ public class Main {
         HashMap<Integer, Integer> mapResult = new HashMap<>();
 
         // Подсчитываю сколько раз встречается каждое число в массиве
-        for (int value:
-             arrayNumbers) {
+        for (int value: arrayNumbers) {
             if (mapResult.containsKey(value)) {
                 mapResult.put(value, mapResult.get(value) + 1);
             } else {
@@ -55,13 +54,13 @@ public class Main {
         // Ищу числа, которые встретились минимальное количество раз
         int minCount = 2_147_483_647;
         ArrayList<Integer> result = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry:
-                mapResult.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry: mapResult.entrySet()) {
             if (minCount >= entry.getValue()) {
                 minCount = entry.getValue();
                 result.add(entry.getKey());
             }
         }
+
         System.out.println("Наименьшее число раз встречаются следующие числа: " + result.toString());
     }
 
