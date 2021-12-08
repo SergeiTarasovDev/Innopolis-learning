abb32d3bb038a6aba1593dd3eab05fd058d83b9b
Sergei Tarasov <www.neon@mail.ru>
2021-12-08 17:51:53 +0500

    add homework16

---
+++ Homework16/src/list/LinkedList.java
@@ -0,0 +1,66 @@
+package list;
+
+/**
+ * 11.11.2021
+ * 17. LinkedList
+ *
+ * @author Sidikov Marsel (First Software Engineering Platform)
+ * @version v1.0
+ */
+public class LinkedList<T> {
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
+
+    private Node<T> first;
+    private Node<T> last;
+    private int size;
+
+    public void add(T element) {
+        // создаю новый узел
+        Node<T> newNode = new Node<>(element);
+        if (size == 0) {
+            first = newNode;
+        } else {
+            last.next = newNode;
+        }
+        last = newNode;
+        size++;
+    }
+
+    public void addToBegin(T element) {
+        Node<T> newNode = new Node<>(element);
+
+        if (size == 0) {
+            last = newNode;
+        } else {
+            newNode.next = first;
+        }
+        first = newNode;
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public T get(int index) {
+        if (size == 0 || index >= size || index < 0) {
+            return null;
+        }
+
+        Node<T> current = first;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        return current.value;
+    }
+
+}
